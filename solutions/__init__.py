2c5dfb5dee868a50a8e0ea9eb51795cfb7210513
Celelibi <celelibi@gmail.com>
2022-12-05 08:06:32 +0100

    solutions: move module name creation out of try
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,8 +1,8 @@
 import importlib
 
 for i in range(1, 26):
+    name = "solutions.day%02d" % i
     try:
-        name = "solutions.day%02d" % i
         importlib.import_module(name)
     except ModuleNotFoundError as e:
         if e.name != name:
