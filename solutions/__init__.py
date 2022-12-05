ead9e39fb0ee970ef4cd79d0b380593d1846259f
Celelibi <celelibi@gmail.com>
2022-12-05 08:51:50 +0100

    Move solutions to directory 2022
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,9 +1,21 @@
 import importlib
+import os
 
-for i in range(1, 26):
-    name = "solutions.day%02d" % i
+solutions_path = os.path.dirname(os.path.relpath(__file__))
+
+for e in os.listdir(os.path.join(solutions_path, "..", "solutions")):
     try:
-        importlib.import_module(name)
-    except ModuleNotFoundError as e:
-        if e.name != name:
-            raise
+        year = int(e)
+    except ValueError:
+        continue
+
+    if year < 2015:
+        continue
+
+    for i in range(1, 26):
+        name = "solutions.%d.day%02d" % (year, i)
+        try:
+            importlib.import_module(name)
+        except ModuleNotFoundError as e:
+            if e.name != name:
+                raise
