cb4765e45f2861b5cd547bc77806a2a53341216a
Celelibi <celelibi@gmail.com>
2022-12-11 12:13:14 +0100

    solutions: day11 v2
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- solutions/2022/day11.py
+++ solutions/2022/day11.py
@@ -9,31 +9,35 @@ from libadventofcode import registry
 class Solver(base.Solver):
     def solve(self, data, rounds, relief=3):
         r = re.compile(r"""Monkey .*:
-  Starting items: (.*)
-  Operation: new = (.*)
-  Test: divisible by (.*)
-    If true: throw to monkey (.*)
-    If false: throw to monkey (.*)""")
+  Starting items: (?P<items>.*)
+  Operation: new = (?P<op>.*)
+  Test: divisible by (?P<div>.*)
+    If true: throw to monkey (?P<iftrue>.*)
+    If false: throw to monkey (?P<iffalse>.*)""")
 
-        rules = []
-        hold = []
+        monkeys = []
         for rule in data.split("\n\n"):
-            m = r.match(rule)
-            hold.append([int(i) for i in m.group(1).split(", ")])
-            op, div, iftrue, iffalse = m.group(2, 3, 4, 5)
-            div, iftrue, iffalse = int(div), int(iftrue), int(iffalse)
-            rules.append((op, div, iftrue, iffalse))
+            m = r.match(rule).groupdict()
+            m["items"] = [int(i) for i in m["items"].split(", ")]
+            m.update({k: int(m[k]) for k in ("div", "iftrue", "iffalse")})
+            monkeys.append(m)
 
-        proddiv = math.prod([div for _, div, _, _ in rules])
+        proddiv = math.prod([m["div"] for m in monkeys])
 
-        business = [0] * len(rules)
+        business = [0] * len(monkeys)
+
         for _ in range(rounds):
-            for monkey, (items, (op, div, iftrue, iffalse)) in enumerate(zip(hold, rules)):
-                hold[monkey] = []
-                business[monkey] += len(items)
-                for item in items:
-                    new = eval(op, {"old": item}) // relief % proddiv
-                    hold[iftrue if new % div == 0 else iffalse].append(new)
+            for i, m in enumerate(monkeys):
+                business[i] += len(m["items"])
+
+                for item in m["items"]:
+                    new = eval(m["op"], {"old": item}) // relief % proddiv
+                    if new % m["div"] == 0:
+                        monkeys[m["iftrue"]]["items"].append(new)
+                    else:
+                        monkeys[m["iffalse"]]["items"].append(new)
+
+                m["items"] = []
 
         return math.prod(sorted(business)[-2:])
 
