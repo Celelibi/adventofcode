3d84785d6ab628c5f56102a39012d8dd6b35b7e7
Celelibi <celelibi@gmail.com>
2022-12-15 14:55:11 +0100

    solutions: day15 with timers
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- solutions/2022/day15.py
+++ solutions/2022/day15.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from libadventofcode import base
 from libadventofcode import registry
+from libadventofcode.utils import Timer
 
 
 
@@ -41,32 +42,42 @@ class Solver(base.Solver):
 
         # rem[y, s] is the remaining distance from sensor s once row y is reached
         # Can be negative if row y is out of reach of sensor s
-        yrange = np.arange(bound + 1).reshape(-1, 1)
-        rem = dist - np.abs(sensors[:, 1] - yrange)
+        with Timer("rem"):
+            yrange = np.arange(bound + 1).reshape(-1, 1)
+            rem = dist - np.abs(sensors[:, 1] - yrange)
 
         # range_from[y, s]..range_to[y, s] is the range of x values reached by sensor s on row y
-        ranges_from = sensors[:, 0] - rem
-        ranges_to = sensors[:, 0] + rem + 1
-        np.clip(ranges_from, 0, bound, out=ranges_from)
-        np.clip(ranges_to, 0, bound, out=ranges_to)
+        with Timer("ranges"):
+            ranges_from = sensors[:, 0] - rem
+            ranges_to = sensors[:, 0] + rem + 1
+            np.clip(ranges_from, 0, bound, out=ranges_from)
+            np.clip(ranges_to, 0, bound, out=ranges_to)
 
         # Sort those ranges by increasing begining values
-        idx = np.argsort(ranges_from, axis=1)
-        ranges_from = np.take_along_axis(ranges_from, idx, axis=1)
-        ranges_to = np.take_along_axis(ranges_to, idx, axis=1)
+        with Timer("sort"):
+            with Timer("sort only"):
+                idx = np.argsort(ranges_from, axis=1)
+            with Timer("take_along_axis from"):
+                ranges_from = np.take_along_axis(ranges_from, idx, axis=1)
+            with Timer("take_along_axis to"):
+                ranges_to = np.take_along_axis(ranges_to, idx, axis=1)
 
-        all_ranges = np.stack((ranges_from, ranges_to), axis=-1)
-        ranges_from = ranges_to = None
+        with Timer("all_ranges"):
+            all_ranges = np.stack((ranges_from, ranges_to), axis=-1)
+            ranges_from = ranges_to = None
 
         # Move the begining of ranges such that it never overlaps with a previous range
-        cummax_to = np.maximum.accumulate(all_ranges[:, :, 1], axis=1)
-        np.maximum(all_ranges[:, 1:, 0], cummax_to[:, :-1], out=all_ranges[:, 1:, 0])
-        cummax_to = None
+        with Timer("cummax"):
+            cummax_to = np.maximum.accumulate(all_ranges[:, :, 1], axis=1)
+            np.maximum(all_ranges[:, 1:, 0], cummax_to[:, :-1], out=all_ranges[:, 1:, 0])
+            cummax_to = None
 
-        # Normalize range ends so that intervals never cross and stay in bounds
-        np.maximum(all_ranges[:, :, 0], all_ranges[:, :, 1], out=all_ranges[:, :, 1])
+        # Normalize range ends so that intervals never cross
+        with Timer("Normalize ends"):
+            np.maximum(all_ranges[:, :, 0], all_ranges[:, :, 1], out=all_ranges[:, :, 1])
 
-        gap = (all_ranges[:, :-1, 1] < all_ranges[:, 1:, 0])
+        with Timer("Gap after"):
+            gap = (all_ranges[:, :-1, 1] < all_ranges[:, 1:, 0])
 
         y, i = np.argwhere(gap)[0]
         return np.dot([all_ranges[y, i, 1], y], [bound, 1])
